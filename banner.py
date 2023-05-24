6f1351caf375e6fb9dd05c98db0bf759541e5c57
Berlin Patience <starsberlin@gmail.com>
2023-05-24 13:18:43 -0400

    first day creating a banner

---
+++ banner.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Welcome to ban Generator.")
+
+#2. Ask the user for the city that they grew up in
+city = input("which city did you grow up in? \n")
+
+#3. Ask the user for the name of a pet.
+pet = input("what's the name of a pet you had? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is " + " " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line
