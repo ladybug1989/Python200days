79b28c0d87c5231ca853c4eb19b8b9f595b22a2e
Berlin Patience <starsberlin@gmail.com>
2023-06-06 23:33:05 -0400

    Day 3: learning about operators and work flow

---
+++ BMI.py
@@ -0,0 +1,18 @@
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you are normal weight." )
+elif bmi < 30:
+    print(f"your bmi is {bmi}, you are overweight. ")
+
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, you are obese.")
+
+else:
+    print(f"Your bmi is {bmi}, your are clinically obese.")
+
